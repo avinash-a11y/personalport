e44c5463052f35a32211eeb9483615d5e153646d
avinash-a11y <72777973+avinash-a11y@users.noreply.github.com>
2025-01-26 20:12:20 +0530

    Update index.jsx

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -108,7 +108,7 @@ function HeroSection() {
               <div>
                 <span className="ml-4 lg:ml-8 mr-2 text-white">name:</span>
                 <span className="text-gray-400">{`'`}</span>
-                <span className="text-amber-300">Anubhav Chaturvedi</span>
+                <span className="text-amber-300">Avinash Gupta</span>
                 <span className="text-gray-400">{`',`}</span>
               </div>
               <div className="ml-4 lg:ml-8 mr-2">
